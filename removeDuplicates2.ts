4e6eef4f059a03ebc2638d86ef37dab3d60ac9b6
NestorAntony <nestor.antony.farias.souza@gmail.com>
2025-11-10 10:42:32 -0400

    otimizacao do exercicio removeDuplicates2(MEDIO)

--- removeDuplicates2.ts
+++ removeDuplicates2.ts
@@ -1,56 +1,12 @@
 function removeDuplicates2(nums: number[]): number {
-    let indice = 0;
-    let duplicata = 0;
-    let j = 0;
-
-    let valoresNoVetor = 0;
-
-    const tamanho = nums.length
-
-
-    const vector = new Array(tamanho)
-
-    for(let k = 0; k<tamanho; k++){ //copiando nums para um vetor temporário.
-        vector[k] = nums[k]
+    let i = 0;
+    
+    for(const ele of nums){
+        if( i==0 || i==1 || ele != nums[i-2]){
+            nums[i] = ele;
+            i++
+        }
     }
 
-    for(let i=0; i< tamanho; i++){
-        j = i+1;
-
-        if(vector[i] == vector[j]){
-            duplicata ++;
-            j++;
-
-            while(vector[i] == vector[j]){
-                duplicata++;
-                j++;
-            }
-            let diferenca = j-i;
-
-            if(diferenca > 1){
-                for(let l = 0; l<2; l++){
-                    nums[indice] = vector[i+l];
-                    indice++;
-                    valoresNoVetor++;
-                }
-
-                i = j-1;
-            }
-            else{
-                nums[indice] = vector[i];
-                indice++;
-                nums[indice] = vector[j];
-                indice++;
-
-                valoresNoVetor = valoresNoVetor + 2;
-            }
-        }
-        else{
-            nums[indice] = vector[i];
-            indice ++;
-            valoresNoVetor++;
-        }
-        duplicata = 0;
-    }
-    return valoresNoVetor;
+    return i;
 };
